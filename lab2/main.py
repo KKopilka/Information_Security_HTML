bee9e1508ce3bf98442f744b3e725909c57f210b
Yana <yana.stepanckova@yandex.ru>
2021-10-04 19:29:23 +0300

    добавлена 1 лаба

--- lab2/main.py
+++ lab2/main.py
@@ -1,8 +1,9 @@
 import random
 def change_password(new_password):
     n = 5
+    mas = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
     for i in range(n - 1):
-        if new_password[i] == new_password[i + 1]:
+        if (new_password[i] == new_password[i + 1]) and new_password[i] == :
             return True
     return False
 result = ""

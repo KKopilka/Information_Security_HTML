aab43e44b85a042a35d8dd26b799098d033302c1
Yana <yana.stepanckova@yandex.ru>
2021-10-04 19:31:48 +0300

    lab2

--- lab2/main.py
+++ lab2/main.py
@@ -3,7 +3,7 @@ def change_password(new_password):
     n = 5
     mas = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
     for i in range(n - 1):
-        if (new_password[i] == new_password[i + 1]) and new_password[i] == :
+        if (new_password[i] == new_password[i + 1]):
             return True
     return False
 result = ""
